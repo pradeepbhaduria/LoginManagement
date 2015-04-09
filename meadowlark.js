6c96e90613adecc066d59e18f980a2468e6f336c
PRADEEP BHADURIA <prbhadur@cisco.com>
2015-04-09 09:56:10 +0530

    Moved fortune cookie functionality into module

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,7 @@
 /*global console, require, process, __dirname */
 
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 // set up handlebars view engine
@@ -20,33 +20,14 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     'use strict';
-    //res.type('text/plain');
-    //res.send('Meadowlark Travel');
     console.log("rendering Home");
     res.render("home");
 });
 
-var fortunes = [
-
-"Conquer your fears or they will conquer you.",
-
-"Rivers need springs.",
-
-"Do not fear what you don't know.",
-
-"You will have a pleasant surprise.",
-
-"Whenever possible, keep it simple.",
-
-];
 app.get('/about', function (req, res) {
     'use strict';
-    //res.type('text/plain');
-
-    //res.send('About Meadowlark Travel');
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
     res.render('about', {
-        fortune: randomFortune
+        fortune: fortune.getFortune()
     });
 
 
@@ -55,11 +36,7 @@ app.get('/about', function (req, res) {
 
 app.use(function (req, res) {
     'use strict';
-    //res.type('text/plain');
-
     res.status(404);
-
-    // res.send('404 - Not Found');
     res.render('404');
 
 });
@@ -70,11 +47,7 @@ app.use(function (err, req, res, next) {
     'use strict';
     console.error(err.stack);
 
-    //res.type('text/plain');
-
     res.status(500);
-
-    // res.send('500 - Server Error');
     res.render('500');
 
 });
