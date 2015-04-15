66918caed6a01c4f985e262cb7b0fde9f268d6c6
PRADEEP BHADURIA <prbhadur@cisco.com>
2015-04-15 11:59:47 +0530

    added form handling

--- meadowlark.js
+++ meadowlark.js
@@ -3,12 +3,24 @@
 
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 var app = express();
 
 // set up handlebars view engine
 
 var handlebars = require('express3-handlebars').create({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            'use strict';
+            if (!this._sections) {
+                this._sections = {};
+            }
+
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 });
 
 app.engine('handlebars', handlebars.engine);
@@ -19,15 +31,52 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+
 app.use(function (req, res, next) {
     'use strict';
     res.locals.showTests = app.get('env') !== 'production' &&
-
         req.query.test === '1';
-
     next();
 
 });
+
+app.use(require('body-parser')());
+app.get('/newsletter', function (req, res) {
+    'use strict';
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+
+    res.render('newsletter', {
+        csrf: 'CSRF token goes here'
+    });
+
+});
+
+app.post('/process', function (req, res) {
+    'use strict';
+    console.log('Form (from querystring): ' + req.query.form);
+
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+
+    console.log('Name (from visible form field): ' + req.body.name);
+
+    console.log('Email (from visible form field): ' + req.body.email);
+
+    //res.redirect(303, '/thank-you');
+    console.log(req.xhr);
+    console.log(req.accepts('json,html'));
+
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        // if there were an error, we would send { error: 'error description' }
+        res.send({
+            success: true
+        });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
 app.get('/', function (req, res) {
     'use strict';
     console.log("Request params:", req.params);
@@ -48,14 +97,38 @@ app.get('/about', function (req, res) {
 });
 
 app.get('/tours/hood-river', function (req, res) {
-
+    'use strict';
     res.render('tours/hood-river');
 
 });
 
 app.get('/tours/request-group-rate', function (req, res) {
-
+    'use strict';
     res.render('tours/request-group-rate');
+
+});
+
+app.get('/contest/vacation-photo', function (req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(),
+        month: now.getMonth()
+    });
+
+});
+
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        if (err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 
 });
 
